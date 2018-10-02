496fcca7d4644337885133c80f414c463df38c48
asifhussain <hoosayn@gmail.com>
2018-10-02 22:21:38 +0300

    3 microservices

---
+++ Donor/src/main/java/org/ngo/Donor/DonorApplication.java
@@ -0,0 +1,12 @@
+package org.ngo.Donor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonorApplication.class, args);
+	}
+}
