f97b591953a7c9bed051fbcee27f47c34689fb47
Ashif Hussain Shaikh <ashaikh@elm.com.sa>
2018-10-03 13:54:51 +0300

    db connection, liquibase and lombok

---
+++ Registration/src/main/java/org/ngo/entity/User.java
@@ -0,0 +1,29 @@
+package org.ngo.entity;
+
+import lombok.Data;
+
+import javax.annotation.sql.DataSourceDefinition;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="NGO_USER")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
+    @Column(name = "USER_NAME")
+    private String username;
+
+    @Size(min = 9, max = 99)
+    private String password;
+
+}
