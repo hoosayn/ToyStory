3b9a6bf0565852d3e3e6614804feb741c4d81044
Ashif Hussain Shaikh <ashaikh@elm.com.sa>
2018-10-05 16:44:48 +0300

    implement restcall

---
+++ Registration/src/main/java/org/ngo/Registration/core/UserRepository.java
@@ -0,0 +1,9 @@
+package org.ngo.registration.core;
+
+import org.ngo.registration.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
