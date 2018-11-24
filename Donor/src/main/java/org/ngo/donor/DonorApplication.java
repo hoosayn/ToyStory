8e05738b40e6533027d8b84022bb2d975b4521fa
Ashif Hussain Shaikh <ashaikh@elm.com.sa>
2018-11-24 11:33:40 +0300

    discovery service

--- Donor/src/main/java/org/ngo/donor/DonorApplication.java
+++ Donor/src/main/java/org/ngo/donor/DonorApplication.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
@@ -22,6 +23,7 @@ import java.util.Optional;
 @SpringBootApplication
 @EnableBinding(Processor.class)
 @EnableCaching
+@EnableDiscoveryClient
 public class DonorApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DonorApplication.class);
--- Registration/src/main/java/org/ngo/registration/RegistrationApplication.java
+++ Registration/src/main/java/org/ngo/registration/RegistrationApplication.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableBinding(Processor.class)
+@EnableDiscoveryClient
 public class RegistrationApplication extends SpringBootServletInitializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationApplication.class);
